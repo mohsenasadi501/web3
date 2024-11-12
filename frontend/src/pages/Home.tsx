1275db74457ea7b4f5eccd87cb0bf4df3216cfef
Mohsen Asadi <mohsenasadi501@yahoo.com>
2024-11-12 22:05:22 +0100

    add

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import EtherumService from '../services/EtherumService'
+import { Button, Box, TextField } from '@mui/material';
 
 const Home: React.FC = () => {
-    return <h1>Home</h1>;
+    const [inputValue, setInputValue] = useState('');
+    const [walletAddress, setWalletAddress] = useState('');
+    const [responseValue, setResponseValue] = useState('Mohsen Asadi');
+    const [walletBalanceValue, setWalletBalanceValue] = useState('0');
+
+    const handleChange = (event: any) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleWalletAddressChange = (event: any) => {
+        setWalletAddress(event.target.value);
+    };
+
+    const fetchData = () => {
+        EtherumService.read(inputValue)
+            .then(response => {
+                console.log(response);
+                setResponseValue(`The name is: ${response.name} and the symbol is: ${response.symbol}`);
+            })
+    }
+    const fetchWalletBalance = () => {
+        EtherumService.read(walletAddress)
+            .then(response => {
+                setResponseValue(`The balance is: ${response}`);
+            })
+    }
+
+    return (<>
+        <Box
+            component="form"
+            sx={{ '& > :not(style)': { m: 1, width: '50ch', marginTop: '30px' } }}
+            noValidate
+            autoComplete="off">
+            <h3>Smart Contarct Details:</h3>
+            <TextField id="outlined-basic"
+                label="Contarct Address"
+                variant="outlined"
+                value={inputValue}
+                required
+                onChange={handleChange} />
+            <Button
+                variant="outlined"
+                onClick={fetchData}>Fetch Data</Button>
+            <div>{responseValue}</div>
+        </Box >
+        <br />
+        <hr />
+        <Box
+            component="form"
+            sx={{ '& > :not(style)': { m: 1, width: '50ch', marginTop: '30px' } }}
+            noValidate
+            autoComplete="off">
+            <h3>Wallet Address Balance:</h3>
+            <TextField id="outlined-basic"
+                label="Wallet Address"
+                variant="outlined"
+                value={walletAddress}
+                required
+                onChange={handleWalletAddressChange} />
+            <Button
+                variant="outlined"
+                onClick={fetchWalletBalance}>Get Balance</Button>
+            <div>{walletBalanceValue}</div>
+        </Box >
+    </>
+
+    );
 }
 export default Home;
---
+++ frontend/src/services/EtherumService.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+class EtherumService {
+    async read(_address: string): Promise<any> {
+        return axios.post('http://localhost:3000/api/etherum/read', { address: _address })
+            .then(response => {
+                console.log(response.data);
+                return response.data;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    async readBalance(_address: string): Promise<any> {
+        return axios.get(`http://localhost:3000/api/etherum/balance?address=${_address}`)
+            .then(response => {
+                console.log(response.data);
+                return response.data;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+}
+export default new EtherumService();
